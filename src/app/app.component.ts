ca46fa2d8cf9d3e3d456acefb2e3832f71185c25
Braa Aboarshed <babwarshy@gmail.com>
2022-10-27 17:59:20 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculator';
+  result =''
+
+  getAction(char:any){
+    this.result += char
+  }
+
+  reset(){
+    this.result =''
+  }
+  equal(){
+    this.result = eval(this.result).toFixed(1)
+  }
 }
